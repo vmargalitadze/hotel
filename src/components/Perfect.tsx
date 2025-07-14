5ba1ace14f3ebe24b987f0a55537382dc2dd8506
vaqsi1990 <vaqsii23@gmail.com>
2025-07-14 15:34:34 +0400

    perfect compo

--- src/components/Perfect.tsx
+++ src/components/Perfect.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
-
+import React from "react";
+import Image from "next/image";
 function Perfect() {
   return (
-   <div className="">
-    
-   </div>
-  )
+    <section className="w-full py-20 bg-white">
+      <div className="flex flex-col md:flex-row items-start justify-between px-4 md:px-12 lg:px-20 gap-12">
+        {/* მარცხენა: დიდი სურათი */}
+        <div className="w-full md:w-[60%]">
+          <Image
+            src="/perfect/polina-kuzovkova-dh3ykciekro-unsplash.webp"
+            alt="სურათი"
+            width={500}
+            height={700}
+            className="w-full h-[900px] object-cover rounded-xl shadow-lg"
+          />
+        </div>
+
+        {/* მარჯვენა: სურათი + ტექსტი ცენტრში */}
+        <div className="w-full md:w-[35%] justify-center mt-24 flex flex-col items-center md:items-center text-center">
+          <h1 className="text-[30px] font-cormorant font-bold mb-6">
+            სრულყოფილი გაქცევა გელოდება
+          </h1>
+
+          <div className="relative w-full max-w-[350px] h-64 md:h-[400px] rounded-xl overflow-hidden shadow-lg mb-4 group">
+            <Image
+              src="/perfect/pratik-bachhav-xtae1e3si6s-unsplash.webp"
+              alt="სურათი"
+              fill
+              className="object-cover"
+            />
+            <div className="absolute inset-0 bg-black/20 group-hover:bg-black/30 transition duration-300" />
+          </div>
+
+          <p className="text-gray-600 text-[18px] font-cormorant">
+            გაიღვიძეთ განსაცვიფრებელი ხედებით, დაისვენეთ მყუდრო კოტეჯებში და
+            შეისწავლეთ თვალწარმტაცი ბილიკები, ჩანჩქერები და ველური ბუნება.
+            თავგადასავლებისთვის თუ დასვენებისთვის, ეს იდეალური ადგილია
+            დასასვენებლად.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Perfect
+export default Perfect;
